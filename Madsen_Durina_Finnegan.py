a2dc5df4fadd53b6906895ebd5361ce4ca35988c
Ciaran Finnegan <ciaran@feefinnegan.com>
2019-10-13 22:21:33 +0100

    Sunday 13th Changes - final
    Should be the completed CA Two code and report

--- Madsen_Durina_Finnegan.py
+++ Madsen_Durina_Finnegan.py
@@ -80,7 +80,6 @@ def MainProg_CATwo():
 
 	# Call RandomForest with full Spruce data Feature Set
 	# Output results to both console and in graphical form
-	# 
 	ImplementTunedRandomForestAlgorithm(X_train, X_test, Y_train, Y_test, best_estimator, X)
 
 	# The previous function provides the analyst with the Spruce dataset featurs lists in 
@@ -194,12 +193,11 @@ def PreSplitDataManipulation(dataset, datasetDescription):
 
 
 	# Pre-Split Data Preparation
-	# Hidden missing values - check the zeroes - we already checked for NULL
-	#print(final_data.head(10))
-	#Elevation	Slope	Horizontal_Distance_To_Hydrology	Vertical_Distance_To_Hydrology	Horizontal_Distance_To_Roadways	Horizontal_Distance_To_Fire_Points
-	SpruceFeatureCheckListForZeroValues = ['Elevation','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways','Horizontal_Distance_To_Fire_Points']
+	# Hidden missing values - check the zeroes 
+	SpruceFeatureCheckListForZeroValues = ['Elevation','Slope','Horizontal_Distance_To_Hydrology','Vertical_Distance_To_Hydrology',
+																'Horizontal_Distance_To_Roadways','Horizontal_Distance_To_Fire_Points']
 	print("\n\t# Rows in {1} dataframe {0}".format(len(final_data), datasetDescription))
-	# It would not seem logical that any of the first six colums in the Spruce dataset have a zero value
+	# Check if any of the first six colums in the Spruce dataset have a zero value
 	# This loop checks if there are any zero values
 	# If there were any zero values the user would determine the appropriate follow up action
 	for feature in SpruceFeatureCheckListForZeroValues:
@@ -232,8 +230,7 @@ def CheckDatasetForCorrelation(dataset, dataDescription):
 	
 def CreateLableAndFeatureSet(final_data, dataDescription):
 
-	# Check distribution of survived and died in cleaned dataset
-	# Check that the observation of any outcome is not too rare
+	# Check distribution of Spruce and Other Tree types cleaned dataset
 	num_obs = len(final_data)
 	num_true = len(final_data.loc[final_data['Tree_Type'] == 1]) # Spruce Tree = True
 	num_false = len(final_data.loc[final_data['Tree_Type'] == 0]) # Spruce Tree = False
@@ -251,8 +248,7 @@ def CreateLableAndFeatureSet(final_data, dataDescription):
 	print("\n\tFeatured + Labeled - {} Dataset Head Rows X + Y : \n".format(dataDescription))
 	print(X.head(2))
 	print(Y.head(2))
-	# Pause
-	#anykey = input("Press any key..")
+
 
 	return X, Y
 
@@ -264,14 +260,7 @@ def NormaliseTrainingData(X, dataDescription):
 	# Normalizing numerical features so that each feature has mean 0 and variance 1
 	feature_scaler = StandardScaler()
 	X_scaled = feature_scaler.fit_transform(X)
-	#print("\nPre-Scaled Features - {} Dataset Head Rows : \n".format(dataDescription))
-	#print(X.head(3))
-	# Pause
-	#anykey = input("Press any key..")
-	#print("\nPost-Scaled Features - {} Dataset Head Rows : \n".format(dataDescription))
-	#print(X_scaled.view())
-	# Pause
-	#anykey = input("Press any key..")
+
 
 	return X_scaled
 
@@ -317,8 +306,7 @@ def ImplementOverSampling(X_train,Y_train):
 	X_train,Y_train = smote.fit_sample(X_train,Y_train)
 
 	print("Number of observations in each class after oversampling (training data): \n", pd.Series(Y_train).value_counts())
-	# Pause
-	# anykey = input("Press any key..")
+
 
 	return X_train,Y_train
 
@@ -332,16 +320,28 @@ def TuneRandomForestAlgorithm(X_train, Y_train):
 	scoring = 'recall' when you want to minimize false negatives
 	scoring = 'precision' when you want to minimize false positives
 	scoring = 'f1' when you want to balance false positives and false negatives (place equal emphasis on minimizing both)
+
+	This code was run to determine the optimum parameters for the RandomForest algorithm
+	I took a considerable amount of time to execute but he results are described in 
+	our analysis report.
+
 	"""
 
 	# Tuning the random forest parameter 'n_estimators' using Grid Search
 	rfc = RandomForestClassifier(criterion='entropy', max_features='auto', random_state=1)
+	# We check all scoring options in the FOR LOOP below
 	scoreOptions = ['accuracy','recall','precision','f1']
-	#grid_param = {'n_estimators': [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700]}
-	grid_param = {'n_estimators': [10,50,100,150]}
+	# We create a broade range of option for the possible final 'n_estimator' value
+	grid_param = {'n_estimators': [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700]}
+
 
 	print("\n\tRunning Grid Search Cross Validation tuning..")
 
+	# Check all scoring options
+	# We are looking to minimise False Negatives and hence are primarily interested in the values
+	# for the 'recall' option.
+	# However finding the other 'n_estimator' suggested parameters will allow us to carry out some
+	# comparative model training
 	for score in scoreOptions:
 		gd_sr = GridSearchCV(estimator=rfc, param_grid=grid_param, scoring=score, cv=5)
 
@@ -352,20 +352,14 @@ def TuneRandomForestAlgorithm(X_train, Y_train):
 		# Determine and display the optimum hyperparameter
 		best_parameters = gd_sr.best_params_
 		print("\n\tOptimum parameter is : {}".format(best_parameters))
-		#print(best_parameters)
-
+		
 		best_result = gd_sr.best_score_ # Mean cross-validated score of the best_estimator
 		print("\n\tOptimum scoring result is : {}".format(best_result))
-		#print(best_result)
-
-	# Pause
-	#anykey = input("Press any key..")
 
 
 def ImplementTunedRandomForestAlgorithm(X_train, X_test, Y_train, Y_test, best_estimator, X):
 
 	# Building random forest using the tuned parameter
-	#rfc = RandomForestClassifier(n_estimators=400, criterion='entropy', max_features='auto', random_state=1)
 	rfc = RandomForestClassifier(n_estimators=best_estimator, criterion='entropy', max_features='auto', random_state=1)
 	rfc.fit(X_train,Y_train)
 	
@@ -373,9 +367,9 @@ def ImplementTunedRandomForestAlgorithm(X_train, X_test, Y_train, Y_test, best_e
 	featimp = pd.Series(rfc.feature_importances_, index=list(X)).sort_values(ascending=False)
 	print("\n\tList of features in dataset by importance to prediction model : \n")
 	print(featimp)
-	# Pause
-	# anykey = input("Press any key..")
 
+	# Run 'predict' function to create a matrix of values to compare agains the 'real world' data
+	# in the Test set
 	Y_pred = rfc.predict(X_test)
 	print("\n\tPrediction Accuracy: ", metrics.accuracy_score(Y_test, Y_pred))
 
@@ -387,10 +381,9 @@ def ImplementTunedRandomForestAlgorithm(X_train, X_test, Y_train, Y_test, best_e
 	print("\n")
 	print("\n\tClassification Report\n")
 	print(metrics.classification_report(Y_test, Y_pred))
-	# Pause
-	# anykey = input("Press any key..")
 
-
+	# Displaying a Confusion Matrix
+	# Graph on screen
 	conf_mat = metrics.confusion_matrix(Y_test, Y_pred)
 	plt.figure(figsize=(8,6))
 	sns.heatmap(conf_mat,annot=True,cmap='Reds', fmt='d')
@@ -398,11 +391,6 @@ def ImplementTunedRandomForestAlgorithm(X_train, X_test, Y_train, Y_test, best_e
 	plt.xlabel("Predicted Class")
 	plt.ylabel("Actual class")
 	plt.show()
-	print('Confusion matrix: \n', conf_mat)
-	#print('TP: ', conf_mat[0,0])
-	#print('TN: ', conf_mat[1,1])
-	#print('FP: ', conf_mat[1,0])
-	#print('FN: ', conf_mat[0,1])
 
 	print('Confusion matrix: \n', conf_mat)
 	print('TP: ', conf_mat[1,1])
@@ -410,8 +398,7 @@ def ImplementTunedRandomForestAlgorithm(X_train, X_test, Y_train, Y_test, best_e
 	print('FP: ', conf_mat[0,1])
 	print('FN: ', conf_mat[1,0])
 
-	# Pause
-	anykey = input("Press any key..")
+
 
 
 def CreateRevisedFeatureSet(dataset):
@@ -440,8 +427,6 @@ def ImplementPCAVisualisation(X_Scaled, Y, dataDescription):
 	print(pca.explained_variance_ratio_)
 	print(sum(pca.explained_variance_ratio_))
 
-	# Pause
-	# anykey = input("Press any key..")
 
 	plt.figure(figsize = (8,6))
 	plt.scatter(x_pca[:,0], x_pca[:,1], c=Y, cmap='plasma')
@@ -480,4 +465,5 @@ def ImplementK_MeansClustering(X_Scaled, x_pca, dataDescription):
 	plt.show()
 
 
+# Run the analysis program
 MainProg_CATwo()
